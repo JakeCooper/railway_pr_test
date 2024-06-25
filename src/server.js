840d9a4d6d0e0618678dc2f1cfb70ca74629719f
Jake Cooper <jake.elijah.cooper@gmail.com>
2024-06-25 00:10:27 -0400

    test

--- src/server.js
+++ src/server.js
@@ -28,7 +28,7 @@ app.get("/", async (req, res) => {
   let timings = await results.map(result => `${result.host}: ${result.time}ms`)
   counter++;
   res.send(
-    `ARG! ${process.env.RAILWAY_REGION}<br/>${timings.join("<br/>")}`,
+    `ParRG! ${process.env.RAILWAY_REGION}<br/>${timings.join("<br/>")}`,
   );
 });
 
